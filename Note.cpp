fb7d4dcc68e1f69a91225a665e7e86c33f77cb3e
jean-edouard-boulanger <jean.edouard.boulanger@gmail.com>
2013-06-01 15:09:19 +0300

    Update Note.cpp
    - Implémentation de toText() pour les sous types NImage, NVideo et NAudio
    - Implémentation de load() pour le type NMedia

--- Note.cpp
+++ Note.cpp
@@ -2,129 +2,178 @@
 
 namespace NM {
 
-
-//-----------------NOTE-------------------
-Note::Note(const unsigned int i, const QString &te)
-    : id(i), title(te), loaded(false), modified(false)
-{
-    this->log();
-}
-
-void Note::log() const {};
-
-
-//-----------------NARTICLE---------------
-NArticle::NArticle(unsigned int i, const QString &te, const QString &txt)
-    : Note(i, te), text(txt)
-{
-    log();
-}
-
-QString  NArticle::toText () const {
-    QString str;
-
-    str = QString::number(id) + "\n";
-    str += "NArticle\n";
-    str += title + "\n\n";
-    str += text;
-
-    return str;
-}
-
-void NArticle::load() {
-    NotesManager& nm = NotesManager::getInstance();
-
-    QFile fd(nm.getWorkspace().path() + "/" + QString::number(id));
-    fd.open(QIODevice::ReadOnly);
-    fd.readLine(); //on passe l'id
-    fd.readLine(); //on passe le type
-    fd.readLine(); //on passe le titre
-    fd.readLine(); //on passe le saut de ligne
-
-    while (!fd.atEnd()) {
-        text += fd.readLine();
+    //-----------------NOTE-------------------
+    Note::Note(const unsigned int i, const QString &te)
+        : id(i), title(te), loaded(false), modified(false)
+    {
+        this->log();
     }
 
-    loaded = true;
-}
-
-void NArticle::log() const {
-    qDebug() << "Article : " << getId() << "\n";
-    //std::cout<< "Article :" << getId() << std::endl;
-}
+    void Note::log() const {};
 
 
-//-----------------Document----------------
-Document::Document(unsigned int i, const QString &te)
-    : Note(i, te)
-{
-    log();
-}
-
-void Document::addNote(Note *n) {
-    notes << n;
-}
-
-void Document::operator <<(Note *n) {
-    notes << n;
-}
-
-void Document::removeNote(Note *n) {
-    notes.removeOne(n);
-}
-
-QString Document::toText () const {
-    QString str;
-
-    str = QString::number(id)
-            + "Document\n"
-            + title + "\n\n";
-
-    //  --------- il faut encore ajouter les notes ----------
-
-    QListIterator<Note*> i(notes);
-     while (i.hasNext()) {
-         str += QString::number(i.next()->getId());
-     }
-
-    return str;
-}
-
-void Document::load() {
-    NotesManager& nm = NotesManager::getInstance();
-
-    QFile fd(nm.getWorkspace().path() + "/" + QString::number(id));
-    fd.open(QIODevice::ReadOnly);
-    fd.readLine(); //on passe l'id
-    fd.readLine(); //on passe le type
-    fd.readLine(); //on passe le titre
-    fd.readLine(); //on passe le saut de ligne
-
-    unsigned int idNote;
-
-    while (!fd.atEnd()) {
-        //----------récupérer le pointeur sur la bonne note
-        idNote = fd.readLine().toUInt();
-        notes<<nm.getNote(idNote);
+    //-----------------NARTICLE---------------
+    NArticle::NArticle(unsigned int i, const QString &te, const QString &txt)
+        : Note(i, te), text(txt)
+    {
+        log();
     }
 
-    loaded = true;
+    QString  NArticle::toText () const {
+        QString str;
+
+        str = QString::number(id) + "\n";
+        str += "NArticle\n";
+        str += title + "\n\n";
+        str += text;
+
+        return str;
+    }
+
+    void NArticle::load() {
+        NotesManager& nm = NotesManager::getInstance();
+
+        QFile fd(nm.getWorkspace().path() + "/" + QString::number(id));
+        fd.open(QIODevice::ReadOnly);
+        fd.readLine(); //on passe l'id
+        fd.readLine(); //on passe le type
+        fd.readLine(); //on passe le titre
+        fd.readLine(); //on passe le saut de ligne
+
+        while (!fd.atEnd()) {
+            text += fd.readLine();
+        }
+
+        loaded = true;
+    }
+
+    void NArticle::log() const {
+        qDebug() << "Article : " << getId() << "\n";
+        //std::cout<< "Article :" << getId() << std::endl;
+    }
+
+
+    //-----------------Document----------------
+    Document::Document(unsigned int i, const QString &te)
+        : Note(i, te)
+    {
+        log();
+    }
+
+    void Document::addNote(Note *n) {
+        notes << n;
+    }
+
+    void Document::operator <<(Note *n) {
+        notes << n;
+    }
+
+    void Document::removeNote(Note *n) {
+        notes.removeOne(n);
+    }
+
+    QString Document::toText () const {
+        QString str;
+
+        str = QString::number(id)
+                + "Document\n"
+                + title + "\n\n";
+
+        //  --------- il faut encore ajouter les notes ----------
+
+        QListIterator<Note*> i(notes);
+         while (i.hasNext()) {
+             str += QString::number(i.next()->getId());
+         }
+
+        return str;
+    }
+
+    void Document::load() {
+        NotesManager& nm = NotesManager::getInstance();
+
+        QFile fd(nm.getWorkspace().path() + "/" + QString::number(id));
+        fd.open(QIODevice::ReadOnly);
+        fd.readLine(); //on passe l'id
+        fd.readLine(); //on passe le type
+        fd.readLine(); //on passe le titre
+        fd.readLine(); //on passe le saut de ligne
+
+        unsigned int idNote;
+
+        while (!fd.atEnd()) {
+            //----------récupérer le pointeur sur la bonne note
+            idNote = fd.readLine().toUInt();
+            notes<<nm.getNote(idNote);
+        }
+
+        loaded = true;
+    }
+
+    void Document::log() const {
+        qDebug() << "Document" << getId() << "\n";
+    }
+
+    //-----------------NMedia----------------
+
+    NMedia::NMedia(unsigned int i, const QString & te, const QString & u, const QString & desc)
+        : Note (i, te), url(u), description(desc)
+    {
+    }
+
+    void NMedia::log() const {
+        qDebug() << "NMedia" << getId() << "\n";
+    }
+
+    void NMedia::load(){
+        NotesManager & nm = NotesManager::getInstance();
+
+        QFile fd(nm.getWorkspace().path() + "/" + QString::number(id));
+        fd.open(QIODevice::ReadOnly);
+        fd.readLine(); //on passe l'id
+        fd.readLine(); //on passe le type
+        fd.readLine(); //on passe le titre
+        fd.readLine(); //on passe le saut de ligne
+
+        url = fd.readLine();
+
+        while(!fd.atEnd()){
+            description += fd.readLine();
+        }
+
+        loaded = true;
+    }
+
+    //-----------------NAudio----------------
+    QString NAudio::toText() const{
+        QString str = QString::number(id) + "\n";
+        str += "NAudio\n";
+        str += title + "\n\n";
+        str += url + "\n";
+        str += description + "\n";
+
+        return str;
+    }
+
+    //-----------------NVideo----------------
+    QString NVideo::toText() const{
+        QString str = QString::number(id) + "\n";
+        str += "NVideo\n";
+        str += title + "\n\n";
+        str += url + "\n";
+        str += description + "\n";
+
+        return str;
+    }
+
+    //-----------------NImage----------------
+    QString NImage::toText() const{
+        QString str = QString::number(id) + "\n";
+        str += "NImage\n";
+        str += title + "\n\n";
+        str += url + "\n";
+        str += description + "\n";
+
+        return str;
+    }
 }
-
-void Document::log() const {
-    qDebug() << "Document" << getId() << "\n";
-}
-
-//-----------------NMedia----------------
-
-NMedia::NMedia(unsigned int i, const QString & te, const QString & u, const QString & desc)
-    : Note (i, te), url(u), description(desc)
-{
-}
-
-void NMedia::log() const {
-    qDebug() << "NMedia" << getId() << "\n";
-}
-
-}
-
