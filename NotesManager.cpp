1a47adb05aa42b7775d2bc17d3d32f05f60ba7fa
jean-edouard-boulanger <jean.edouard.boulanger@gmail.com>
2013-06-01 14:12:48 +0200

    Update NotesManager.cpp
    Ajout des factories Image, Audio et Video à la liste des factories

--- NotesManager.cpp
+++ NotesManager.cpp
@@ -2,136 +2,147 @@
 
 namespace NM {
 
-NotesManager *NotesManager::nmInstance = 0;
+    NotesManager *NotesManager::nmInstance = 0;
 
-NotesManager::NotesManager ()
-    : path(QString("../Ressources/"))
-{
-    QString s("NArticle");
-    NoteFactory *nf = new ArticleFactory(s);
-    factories.insert(s, nf);
+    NotesManager::NotesManager ()
+        : path(QString("../Ressources/"))
+    {
+        QString s("NArticle");
+        NoteFactory *nf = new ArticleFactory(s);
+        factories.insert(s, nf);
 
-    s = "Document";
-    nf = new DocumentFactory(s);
-    factories.insert(s, nf);
+        s = "Document";
+        nf = new DocumentFactory(s);
+        factories.insert(s, nf);
 
-    loadWorkspace(QString("workspace1"));
-}
+        s = "NVideo";
+        nf = new VideoFactory(s);
+        factories.insert(s, nf);
 
-NotesManager & NotesManager::getInstance () {
-    if (nmInstance == 0) {
-        nmInstance = new NotesManager();
+        s = "NAudio";
+        nb = new AudioFactory(s);
+        factories.insert(s, nf);
+
+        s = "NImage";
+        nb = new ImageFactory(s);
+        factories.insert(s, nf);
+
+        loadWorkspace(QString("workspace1"));
     }
 
-    return *nmInstance;
-}
+    NotesManager & NotesManager::getInstance () {
+        if (nmInstance == 0) {
+            nmInstance = new NotesManager();
+        }
 
-void NotesManager::releaseInstance () {
-    if (nmInstance != 0) {
-        delete nmInstance;
+        return *nmInstance;
     }
-    nmInstance = 0;
-}
 
-NotesManager::~NotesManager(){
-    //delete sur les factories, les notes si elles ne sont pas enregistrées...
-    QSet<Note*>::iterator i;
-    for (i = notes.begin(); i != notes.end(); ++i) {
-        if ((*i)->isModified())
-            save(*i);
-        delete *i;
+    void NotesManager::releaseInstance () {
+        if (nmInstance != 0) {
+            delete nmInstance;
+        }
+        nmInstance = 0;
     }
-}
 
-void NotesManager::loadWorkspace(QString fold) {
-    if (fold == "")
-        fold = "workspace1";
+    NotesManager::~NotesManager(){
+        //delete sur les factories, les notes si elles ne sont pas enregistrées...
+        QSet<Note*>::iterator i;
+        for (i = notes.begin(); i != notes.end(); ++i) {
+            if ((*i)->isModified())
+                save(*i);
+            delete *i;
+        }
+    }
 
-    workspace.mkdir(path + fold);   //crée le dossier si jamais il n'existe pas
-    workspace.setPath(path + fold);
+    void NotesManager::loadWorkspace(QString fold) {
+        if (fold == "")
+            fold = "workspace1";
 
-    //parcourt le dossier. Si note : ajout au set notes. Si document, crée un document,
-    //l'ajoute au set notes, lui fait référencer ses notes (les crée au besoin, en not loaded)
+        workspace.mkdir(path + fold);   //crée le dossier si jamais il n'existe pas
+        workspace.setPath(path + fold);
 
-    workspace.setFilter(QDir::Files | QDir::NoSymLinks | QDir::NoDotAndDotDot | QDir::Readable | QDir::Writable);
-    QStringList dir = workspace.entryList();
+        //parcourt le dossier. Si note : ajout au set notes. Si document, crée un document,
+        //l'ajoute au set notes, lui fait référencer ses notes (les crée au besoin, en not loaded)
 
-    //lecture des fichiers
-    for (int i = 0; i < dir.size(); i++) {
-        //affiche le nom du fichier disque correspondant
-        qDebug() <<  workspace.path() + "/" + dir.at(i);
+        workspace.setFilter(QDir::Files | QDir::NoSymLinks | QDir::NoDotAndDotDot | QDir::Readable | QDir::Writable);
+        QStringList dir = workspace.entryList();
 
-        //on déclare un descripteur de fichier, ouvert en read-only
-        QFile fd(workspace.path() + "/" + dir.at(i));
-        fd.open(QIODevice::ReadOnly);
+        //lecture des fichiers
+        for (int i = 0; i < dir.size(); i++) {
+            //affiche le nom du fichier disque correspondant
+            qDebug() <<  workspace.path() + "/" + dir.at(i);
 
-        //on récupère l'id de la note, son type et son titre (chop supprime le dernier caractère de ligne "\n"
-        QString sid = fd.readLine();
-        sid.chop(1);
-        unsigned int id = sid.toUInt(); //vérification du nom ??
-        qDebug() << id;
-        QString type = fd.readLine();
-        type.chop(1);
-        QString title = fd.readLine();
-        title.chop(1);
+            //on déclare un descripteur de fichier, ouvert en read-only
+            QFile fd(workspace.path() + "/" + dir.at(i));
+            fd.open(QIODevice::ReadOnly);
 
-        //ajout à notes de la note qui vient d'être lue, puis fermeture de la note
-        qDebug() << "type :" << type<< " title :" << title;
-        notes << factories[type]->buildNote(id, title);
+            //on récupère l'id de la note, son type et son titre (chop supprime le dernier caractère de ligne "\n"
+            QString sid = fd.readLine();
+            sid.chop(1);
+            unsigned int id = sid.toUInt(); //vérification du nom ??
+            qDebug() << id;
+            QString type = fd.readLine();
+            type.chop(1);
+            QString title = fd.readLine();
+            title.chop(1);
+
+            //ajout à notes de la note qui vient d'être lue, puis fermeture de la note
+            qDebug() << "type :" << type<< " title :" << title;
+            notes << factories[type]->buildNote(id, title);
+
+            fd.close();
+        }
+
+
+    }
+
+    Note* NotesManager::getNote (unsigned int id) {
+        QSet<Note*>::iterator i;
+        for (i = notes.begin(); i != notes.end(); ++i) {
+            if ((*i)->getId() == id)
+                return *i;
+        }
+        return 0;
+    }
+
+    void NotesManager::addArticle (Note*n) {
+        notes<<n;
+    }
+
+    void NotesManager::operator <<(Note *n) {
+        notes<<n;
+    }
+
+    Note & NotesManager::getNewNote(const QString & fact) {
+        if(factories.keys().indexOf(fact) != -1) {
+            Note *na = factories[fact]->buildNewNote();
+            notes<<na;
+            return *na;
+        } else {
+            throw "this factory does not exist";
+        }
+    }
+
+    Note & NotesManager::getNewNArticle () {
+        Note *na = factories["NArticle"]->buildNewNote();
+        notes<<na;
+        return *na;
+    }
+
+    Note & NotesManager::getNewDocument() {
+        Note *na = factories["Document"]->buildNewNote();
+        notes<<na;
+        return *na;
+    }
+
+    void NotesManager::save(const Note * note) const {
+        QFile fd(workspace.path() + "/" + QString::number(note->getId()));
+        fd.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text);
+
+        QTextStream in(&fd);
+        in << note->toText();
 
         fd.close();
     }
-
-
-}
-
-Note* NotesManager::getNote (unsigned int id) {
-    QSet<Note*>::iterator i;
-    for (i = notes.begin(); i != notes.end(); ++i) {
-        if ((*i)->getId() == id)
-            return *i;
-    }
-    return 0;
-}
-
-void NotesManager::addArticle (Note*n) {
-    notes<<n;
-}
-
-void NotesManager::operator <<(Note *n) {
-    notes<<n;
-}
-
-Note & NotesManager::getNewNote(const QString & fact) {
-    if(factories.keys().indexOf(fact) != -1) {
-        Note *na = factories[fact]->buildNewNote();
-        notes<<na;
-        return *na;
-    } else {
-        throw "this factory does not exist";
-    }
-}
-
-Note & NotesManager::getNewNArticle () {
-    Note *na = factories["NArticle"]->buildNewNote();
-    notes<<na;
-    return *na;
-}
-
-Note & NotesManager::getNewDocument() {
-    Note *na = factories["Document"]->buildNewNote();
-    notes<<na;
-    return *na;
-}
-
-void NotesManager::save(const Note * note) const {
-    QFile fd(workspace.path() + "/" + QString::number(note->getId()));
-    fd.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text);
-
-    QTextStream in(&fd);
-    in << note->toText();
-
-    fd.close();
-}
-
 }
