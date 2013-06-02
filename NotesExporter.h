b17d376114ed4e6c21addc8f3acfe929b9789750
jean-edouard-boulanger <jean.edouard.boulanger@gmail.com>
2013-06-02 15:13:23 +0200

    Update NotesExporter.h

--- NotesExporter.h
+++ NotesExporter.h
@@ -1,48 +1,81 @@
 #pragma once
 
 #include <QList>
+#include <QDateTime>
+#include <QMap>
+
 #include "Note.h"
 
 namespace NM{
     class NotesExporter
     {
-        NotesExporter();
-        virtual ~NotesExporter() = 0;
-        QSet<const Note*> ExportedNotes;
+    protected:
+        Note * ExportedNote;
+        NotesExporter(Note * ExportedNote) : ExportedNote(ExportedNote){}
     public:
-        virtual void Export() = 0;
-        virtual QString & getRawExport() = 0;
+        virtual QString getRawExport() = 0;
 
-        void insert(const Note *);
-        void operator<<(const Note*);
-
-        void remove(const Note *);
+        void setExportedNote(Note * ExportedNote);
     };
 
     class textNotesExporter : NotesExporter{
     public:
-        textNotesExporter();
-        void Export();
-        QString & getRawExport();
+        textNotesExporter(Note * ExportedNote) : NotesExporter(ExportedNote){}
+        QString getRawExport();
     };
 
     class HTMLNotesExporter : NotesExporter{
         QString spreadSheet;
     public:
-        HTMLNotesExporter();
+        HTMLNotesExporter(Note * ExportedNote) : NotesExporter(ExportedNote){}
 
-        void setCustomSpreadSheet(QString & css);
+        void setSpreadSheet(QString & css);
+        QString getSpreadSheet(){return spreadSheet;}
 
-        void Export();
-        QString & getRawExport();
+        QString getRawExport();
 
     };
 
     class LaTexNotesExporter : NotesExporter{
     public:
-        LaTexNotesExporter();
+        enum latexDocType{book, article, report};
 
-        void Export();
-        QString & getRawExport();
+        LaTexNotesExporter(Note * ExportedNote) : NotesExporter(ExportedNote){
+            ignoreAudio = true;
+            ignoreVideo = true;
+            ignoreNumbering = false;
+            delimitParts = false;
+            displayDate = false;
+            docType = article;
+
+            titleSize.insert(1, "part");
+            titleSize.insert(2, "section");
+            titleSize.insert(3, "subsection");
+            titleSize.insert(4, "subsubsection");
+        }
+
+        void setDocType(latexDocType docType);
+        latexDocType getDocType() const {return docType;}
+
+        void setIgnoreAudio(bool IgnoreAudio){ignoreAudio = IgnoreAudio;}
+        void setIgnoreVideo(bool IgnoreVideo){ignoreVideo = IgnoreVideo;}
+        void setDisplayDate(bool DisplayDate){displayDate = DisplayDate;}
+        void setIgnoreNumerotation(bool IgnoreNumbering){ignoreNumbering = IgnoreNumbering;}
+        void setDelimitParts(bool DelimitParts){delimitParts = DelimitParts;}
+
+        QString getRawExport();
+    private:
+        latexDocType docType;
+        bool ignoreAudio;
+        bool ignoreVideo;
+        bool displayDate;
+        bool ignoreNumbering;
+        bool delimitParts;
+
+        QMap <int, QString> titleSize;
+
+        QString NoteToLatex(Note * ConvertedNote, unsigned int titleLevel);
+        QString ArticleToLatex(NArticle * ConvertedArticle, unsigned int titleLevel);
+        QString ImageToLatex(NImage * ConvertedImage, unsigned int titleLevel);
     };
 }
