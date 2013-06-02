6e1c761ff2ce2496cb5f5bb0b7f65cfd6f5ff34a
jean-edouard-boulanger <jean.edouard.boulanger@gmail.com>
2013-06-02 22:03:40 +0300

    Update NotesExporter.cpp

--- NotesExporter.cpp
+++ NotesExporter.cpp
@@ -28,18 +28,29 @@ namespace NM{
         {
             NArticle * tArt = dynamic_cast<NArticle*>(ConvertedNote);
             if(tArt){
-                return latexExport + ArticleToLatex(tArt, titleLevel);
+                latexExport += ArticleToLatex(tArt, titleLevel);
             }
         }
         else if(ConvertedNote->getType() == Note::NImage){
             NImage * tIm = dynamic_cast<NImage*>(ConvertedNote);
             if(tIm){
-                return latexExport + ImageToLatex(tIm, titleLevel);
+                latexExport += ImageToLatex(tIm, titleLevel);
             }
         }
-        else
+        else if(!ignoreAudio && ConvertedNote->getType() == Note::NAudio)
         {
-            qDebug() << "Can't export Movie or Audio to Latex format\n";
+            NAudio * tAud = dynamic_cast<NAudio*>(ConvertedNote);
+            if(tAud){
+                qDebug() << tAud->getDescription() << "\n";
+                latexExport += AudioToLatex(tAud, titleLevel);
+            }
+        }
+        else if(!ignoreVideo && ConvertedNote->getType() == Note::NVideo){
+            NVideo * tVid = dynamic_cast<NVideo*>(ConvertedNote);
+            if(tVid){
+                qDebug() << tVid->getDescription() << "\n";
+                latexExport += VideoToLatex(tVid, titleLevel);
+            }
         }
         return latexExport;
     }
@@ -49,9 +60,7 @@ namespace NM{
         if(titleLevel){
             latexExport += "\\" + titleSize[titleLevel] + (!ignoreNumbering ? "" : "*") + "{" + ConvertedArticle->getTitle() + "}";
         }
-        latexExport += "\\paragraph{";
         latexExport += ConvertedArticle->getText() + "\\\\";
-        latexExport += "}\n\n";
 
         return latexExport;
     }
@@ -59,13 +68,41 @@ namespace NM{
     QString LaTexNotesExporter::ImageToLatex(NImage * ConvertedImage, unsigned int titleLevel){
         QString latexExport = "";
         if(titleLevel){
-            latexExport += "\\" + titleSize[titleLevel] + (!ignoreNumbering ? "" : "*") + "{" + ConvertedImage->getTitle() + "}";
+            latexExport += "\\" + titleSize[titleLevel] + (!ignoreNumbering ? "" : "*") + "{" + ConvertedImage->getTitle() + "}\n\n";
         }
-        latexExport += "\\begin{figure}\n";
+        latexExport += "\\begin{figure}[h!]\n";
         latexExport += "\\centering\n";
         latexExport += "\\includegraphics[scale = 1]{" + ConvertedImage->getUrl() + "}\n";
-        latexExport += "\\end{figure}\n";
-        latexExport += ConvertedImage->getDescription() + "\n\n";
+        latexExport += "\\caption{" + ConvertedImage->getDescription() + "}";
+        latexExport += "\\end{figure}\n\n";
+
+        return latexExport;
+    }
+
+    QString LaTexNotesExporter::AudioToLatex(NAudio * ConvertedAudio, unsigned int titleLevel){
+        QString latexExport = "";
+        if(titleLevel){
+            latexExport += "\\" + titleSize[titleLevel] + (!ignoreNumbering ? "" : "*") + "{" + ConvertedAudio->getTitle() + "}\n\n";
+        }
+        latexExport += "\\begin{figure}[h!]\n";
+        latexExport += "\\centering\n";
+        latexExport += "\\includegraphics[scale = 0.5]{aud.jpg}\n";
+        latexExport += "\\caption{Fichier Audio"+ ConvertedAudio->getDescription() +"\\\\Ecouter: "+ ConvertedAudio->getUrl() +"}\n";
+        latexExport += "\\end{figure}\n\n";
+
+        return latexExport;
+    }
+
+    QString LaTexNotesExporter::VideoToLatex(NVideo * ConvertedVideo, unsigned int titleLevel){
+        QString latexExport = "";
+        if(titleLevel){
+            latexExport += "\\" + titleSize[titleLevel] + (!ignoreNumbering ? "" : "*") + "{" + ConvertedVideo->getTitle() + "}\n\n";
+        }
+        latexExport += "\\begin{figure}[h!]\n";
+        latexExport += "\\centering\n";
+        latexExport += "\\includegraphics[scale = 0.5]{vid.jpg}\n";
+        latexExport += "\\caption{Fichier Video : "+ ConvertedVideo->getDescription() +"\\\\Regarder: "+ ConvertedVideo->getUrl() +"}\n";
+        latexExport += "\\end{figure}\n\n";
 
         return latexExport;
     }
@@ -77,11 +114,15 @@ namespace NM{
         latexExport += "\\usepackage[francais]{babel}\n";
         latexExport += "\\usepackage{courier}\n";
         latexExport += "\\usepackage{caption}\n";
+        latexExport += "\\usepackage{graphicx}\n";
         latexExport += "\\usepackage[top=1cm, bottom=2cm, right=1.5cm, left=1.5cm]{geometry}\n\n";
         latexExport += "\\title{"+ ExportedNote->getTitle() +"}\n\n";
+
+         latexExport += "\\date{";
         if(displayDate){
-            latexExport += "\\date{\\date\\today}\n\n";
+            latexExport += "\\date\\today";
         }
+        latexExport += "}";
 
         latexExport += "\\begin{document}\n\n";
         latexExport += "\\maketitle\n\n";
